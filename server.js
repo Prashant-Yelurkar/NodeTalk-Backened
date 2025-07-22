c27e9702359fdca3f0ad234f5a4f2d2ac1256748
Prashant Erappa Yelurkar <83222617+Prashant-Yelurkar@users.noreply.github.com>
2025-07-22 21:21:48 +0530

    done

--- server.js
+++ server.js
@@ -10,43 +10,43 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-const server = http.createServer(app);
-const io = new SocketIO(server, {
-  cors: {
-    origin:  process.env.NODE_ENV === "development"
-    ? process.env.APP_DEVELOPMENT_LINK
-    : process.env.APP_PRODUCTION_LINK, // your frontend origin
-    methods: ['GET', 'POST'],
-  },
-});
+// const server = http.createServer(app);
+// const io = new SocketIO(server, {
+//   cors: {
+//     origin:  process.env.NODE_ENV === "development"
+//     ? process.env.APP_DEVELOPMENT_LINK
+//     : process.env.APP_PRODUCTION_LINK, // your frontend origin
+//     methods: ['GET', 'POST'],
+//   },
+// });
 
 
 
-app.use(CORS({
-  origin:  process.env.NODE_ENV === "development"
-    ? process.env.APP_DEVELOPMENT_LINK
-    : process.env.APP_PRODUCTION_LINK,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+// app.use(CORS({
+//   origin:  process.env.NODE_ENV === "development"
+//     ? process.env.APP_DEVELOPMENT_LINK
+//     : process.env.APP_PRODUCTION_LINK,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+// }));
 
 
 // Middleware
-app.use(express.json());
+// app.use(express.json());
 
 
 
-import authRoute from './auth/authRoute.js';
-import useRoute from './routes/userRoute.js'
-import authenticateToken from './auth/authMiddleware.js';
-import chatRoute from './routes/chatRoute.js'
-import { validateToken } from './auth/authController.js';
-import User from './model/userModel.js';
-import MessageModal from './model/messsageModel.js';
-import chatModel from './model/chatModel.js';
-app.use('/auth', authRoute);
-app.use('/user', authenticateToken  ,useRoute )
-app.use('/chat',authenticateToken, chatRoute)
+// import authRoute from './auth/authRoute.js';
+// import useRoute from './routes/userRoute.js'
+// import authenticateToken from './auth/authMiddleware.js';
+// import chatRoute from './routes/chatRoute.js'
+// import { validateToken } from './auth/authController.js';
+// import User from './model/userModel.js';
+// import MessageModal from './model/messsageModel.js';
+// import chatModel from './model/chatModel.js';
+// app.use('/auth', authRoute);
+// app.use('/user', authenticateToken  ,useRoute )
+// app.use('/chat',authenticateToken, chatRoute)
 
 app.get('/', (req, res) => {
 
@@ -54,97 +54,97 @@ app.get('/', (req, res) => {
 });
 
 
-const ActiveUser =new  Map();
-io.on('connection', async (socket) => {
-  const token = socket.handshake.auth.token;
-  try {
-    const decodedUser = validateToken(token);
-    const user = await User.findOne({ email: decodedUser.email });
-    if (!user) return;
-    ActiveUser.set(decodedUser.id.toString() , socket.id )
-    user.isOnline = true;
-    await user.save();
+// const ActiveUser =new  Map();
+// io.on('connection', async (socket) => {
+//   const token = socket.handshake.auth.token;
+//   try {
+//     const decodedUser = validateToken(token);
+//     const user = await User.findOne({ email: decodedUser.email });
+//     if (!user) return;
+//     ActiveUser.set(decodedUser.id.toString() , socket.id )
+//     user.isOnline = true;
+//     await user.save();
 
-    console.log(`🔌 User connected: ${socket.id}`);
+//     console.log(`🔌 User connected: ${socket.id}`);
 
-    socket.on('message', async (data) => {
-        try {
-            const senderId = validateToken(socket.handshake.auth.token).id;
-            const { chatId, userId, message } = data;
-            console.log(userId , senderId);
+//     socket.on('message', async (data) => {
+//         try {
+//             const senderId = validateToken(socket.handshake.auth.token).id;
+//             const { chatId, userId, message } = data;
+//             console.log(userId , senderId);
     
 
-            let chat;
+//             let chat;
 
-            if (chatId) {
-                chat = await chatModel.findById(chatId);
-            }
+//             if (chatId) {
+//                 chat = await chatModel.findById(chatId);
+//             }
 
 
-            if (!chat) {
-                const members = [senderId, userId].sort(); 
-                chat = await chatModel.findOne({
-                    members: members,
-                    isGroup: false
-                });
-                if (!chat) {
-                    chat = new chatModel({
-                    members: members,
-                    isGroup: false,
-                    lastMessage: message.text
-                    });
+//             if (!chat) {
+//                 const members = [senderId, userId].sort(); 
+//                 chat = await chatModel.findOne({
+//                     members: members,
+//                     isGroup: false
+//                 });
+//                 if (!chat) {
+//                     chat = new chatModel({
+//                     members: members,
+//                     isGroup: false,
+//                     lastMessage: message.text
+//                     });
 
-                    await chat.save(); 
-                }
-            }
+//                     await chat.save(); 
+//                 }
+//             }
             
-                chat.lastMessage = message.text;
-                await chat.save();
+//                 chat.lastMessage = message.text;
+//                 await chat.save();
 
-                const newMessage = new MessageModal({
-                    chatId: chat._id,
-                    sender: senderId,
-                    text: message.text,
-                    type: message.type
-                });
+//                 const newMessage = new MessageModal({
+//                     chatId: chat._id,
+//                     sender: senderId,
+//                     text: message.text,
+//                     type: message.type
+//                 });
 
-                await newMessage.save();
-                const receiverSocketId = ActiveUser.get(userId);
-                console.log(receiverSocketId);
+//                 await newMessage.save();
+//                 const receiverSocketId = ActiveUser.get(userId);
+//                 console.log(receiverSocketId);
                 
-                if(receiverSocketId)
-                    {
-                        io.to(receiverSocketId).emit("receiveMessage", {
-                            chatId:chat?._id,
-                            message:{...message, sender:senderId},
-                        });
-                    }
-            } catch (error) {
-                console.error('Message error:', error);
-            }
-            });
+//                 if(receiverSocketId)
+//                     {
+//                         io.to(receiverSocketId).emit("receiveMessage", {
+//                             chatId:chat?._id,
+//                             message:{...message, sender:senderId},
+//                         });
+//                     }
+//             } catch (error) {
+//                 console.error('Message error:', error);
+//             }
+//             });
 
 
-    socket.on('disconnect', async () => {
-      console.log(`❌ User disconnected: ${socket.id}`);
-      user.isOnline = false;
-      user.lastActive = new Date()
-      await user.save();
-      ActiveUser.delete(user._id.toString());
-    });
+//     socket.on('disconnect', async () => {
+//       console.log(`❌ User disconnected: ${socket.id}`);
+//       user.isOnline = false;
+//       user.lastActive = new Date()
+//       await user.save();
+//       ActiveUser.delete(user._id.toString());
+//     });
 
-  } catch (error) {
-    console.error('Socket auth error:', error.message);
-    socket.disconnect(); 
-  }
-});
+//   } catch (error) {
+//     console.error('Socket auth error:', error.message);
+//     socket.disconnect(); 
+//   }
+// });
 
 
 const connection = async()=>{
     try{
         connectDB(process.env.MONGODB)
         .then(() => {
-           server.listen(port, () => {
+           app.listen(port, () => {
                 console.log(`Server is running at http://localhost:${port}`);
             });
         })
