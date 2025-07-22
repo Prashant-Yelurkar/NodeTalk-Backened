bc744f19b9e6bd4c994a97638e871129d757ad7a
Prashant Erappa Yelurkar <83222617+Prashant-Yelurkar@users.noreply.github.com>
2025-07-22 21:28:18 +0530

    done

--- index.js
+++ index.js
@@ -2,10 +2,26 @@ import express  from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+import connectDB from './db/connect.js';
+
 app.get('/', (req, res) => {
   res.send('Hello from simple Node.js app!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+
+const connection = async()=>{
+    try{
+        connectDB(process.env.MONGODB)
+        .then(() => {
+           app.listen(PORT, () => {
+                console.log(`Server is running at http://localhost:${port}`);
+            });
+        })
+        .catch((error) => console.log(error));
+    }
+    catch(error){
+            console.error("MongoDB Connection Error:", error);
+    }; 
+}
+
+connection()
